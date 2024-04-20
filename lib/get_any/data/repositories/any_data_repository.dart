6289ade0d70b46019753ef6bf07a7f5bd632129f
arthur <arthurguatsaev@gmail.com>
2024-04-20 13:25:51 +0300

    tg @ArthurAlan

--- lib/get_any/data/repositories/any_data_repository.dart
+++ lib/get_any/data/repositories/any_data_repository.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:store_example/get_any/data/api_client/any_client.dart';
 import 'package:store_example/get_any/domain/repositories/any_repository.dart';
 
 class AnyRepository extends BaseAnyRepository {
   @override
-  FutureOr<List<String>> getAnyList(int page, int limit) {
+  FutureOr<List<String>> getAnyList(int page, int limit) async {
+    await Future.delayed(Duration(milliseconds: Random(150).nextInt(800)));
     final x = AnyApiClient.getAnyList(page, limit);
     return (anyList ??= [])..addAll(x);
   }
